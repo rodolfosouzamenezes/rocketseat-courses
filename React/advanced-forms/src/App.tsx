a8f2e630051b5e799071a8d9775b6701b988bc28
rodolfosouzamenezes <rodolfosouzaofc@gmail.com>
2024-05-20 04:14:57 -0300

    feat(advanced-forms): add validation/transformation

--- React/advanced-forms/src/App.tsx
+++ React/advanced-forms/src/App.tsx
@@ -1,12 +1,63 @@
 import { useForm } from "react-hook-form";
 import "./styles/global.css";
 import { useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const createUserFormSchema = z
+  .object({
+    name: z
+      .string()
+      .min(1, "O email é obrigatório")
+      .transform((name) => {
+        return name
+          .trim()
+          .split(" ")
+          .map((word) => {
+            return word[0].toLocaleUpperCase().concat(word.substring(1));
+          })
+          .join(" ");
+      }),
+    email: z
+      .string()
+      .min(1, "O email é obrigatório")
+      .email("Formato de email inválido")
+      .toLowerCase()
+      .refine((email) => {
+        return email.endsWith("@gmail.com");
+      }, "O email precisa ser do Gmail"),
+    password: z
+      .string()
+      .min(6, "A senha precisa de no mínimo 6 caracteres")
+      .max(12, "A senha precisa ter no máximo 12 caracteres"),
+    confirmPassword: z
+      .string()
+      .min(6, "A senha precisa de no mínimo 6 caracteres")
+      .max(12, "A senha precisa ter no máximo 12 caracteres"),
+  })
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+      ctx.addIssue({
+        code: "custom",
+        message: "As senhas não batem",
+        path: ["confirmPassword"],
+      });
+    }
+  });
+
+type CreateUserFormData = z.infer<typeof createUserFormSchema>;
 
 function App() {
-  const [output, setOutput] = useState('')
-  const { register, handleSubmit } = useForm();
+  const [output, setOutput] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateUserFormData>({
+    resolver: zodResolver(createUserFormSchema),
+  });
 
-  const createUser = (data: any) => {
+  const createUser = (data: CreateUserFormData) => {
     setOutput(JSON.stringify(data, null, 2));
   };
 
@@ -17,33 +68,64 @@ function App() {
         className="flex flex-col gap-4 w-full max-w-xs"
       >
         <div className="flex flex-col gap-1">
-          <label htmlFor="">Email</label>
+          <label htmlFor="name">Nome</label>
+          <input
+            type="text"
+            className="border border-zinc-800 bg-zinc-900 text-white shadow-sm rounded h-10 px-3"
+            {...register("name")}
+          />
+          {errors.name && (
+            <span className="text-red-600 text-sm">{errors.name.message}</span>
+          )}
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="email">Email</label>
           <input
             type="email"
             className="border border-zinc-800 bg-zinc-900 text-white shadow-sm rounded h-10 px-3"
             {...register("email")}
           />
+          {errors.email && (
+            <span className="text-red-600 text-sm">{errors.email.message}</span>
+          )}
         </div>
 
         <div className="flex flex-col gap-1">
-          <label htmlFor="">Senha</label>
+          <label htmlFor="password">Senha</label>
           <input
             type="password"
             className="border border-zinc-800 bg-zinc-900 text-white shadow-sm rounded h-10 px-3"
             {...register("password")}
           />
+          {errors.password && (
+            <span className="text-red-600 text-sm">
+              {errors.password.message}
+            </span>
+          )}
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label htmlFor="confirmPassword">Confirmar Senha</label>
+          <input
+            type="password"
+            className="border border-zinc-800 bg-zinc-900 text-white shadow-sm rounded h-10 px-3"
+            {...register("confirmPassword")}
+          />
+          {errors.confirmPassword && (
+            <span className="text-red-600 text-sm">
+              {errors.confirmPassword.message}
+            </span>
+          )}
         </div>
 
         <button
           type="submit"
-          className="bg-emerald-500 rounded font-semibold text-white h-10 hover:bg-emerald-600"
+          className="bg-emerald-500 mt-4 rounded font-semibold text-white h-10 hover:bg-emerald-600"
         >
           Salvar
         </button>
       </form>
-      <pre>
-        {output}
-      </pre>
+      <pre>{output}</pre>
     </main>
   );
 }
